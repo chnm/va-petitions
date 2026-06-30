e1eabe84f20e24bfaef78c8ecdaf8b8f34eed91d
Jason Heppler <jason.heppler@gmail.com>
2026-06-30 14:35:27 -0500

    feat: add ability to geocode counties within django-admin

--- petitions/admin.py
+++ petitions/admin.py
@@ -1,4 +1,7 @@
-from django.contrib import admin
+import time
+
+from django.contrib import admin, messages
+from geopy.geocoders import Nominatim
 from import_export.admin import ImportExportModelAdmin
 from unfold.admin import ModelAdmin
 from unfold.contrib.import_export.forms import ExportForm, ImportForm
@@ -6,13 +9,47 @@ from unfold.contrib.import_export.forms import ExportForm, ImportForm
 from .models import County, Petition, Subject
 from .resources import PetitionResource
 
+STATE_NAMES = {
+    'VA': 'Virginia',
+    'WV': 'West Virginia',
+    'KY': 'Kentucky',
+    'PA': 'Pennsylvania',
+}
+
+
+@admin.action(description='Geocode selected counties (fill lat/lng via Nominatim)')
+def geocode_counties(modeladmin, request, queryset):
+    geolocator = Nominatim(user_agent='va-petitions-admin')
+    success, failed = 0, 0
+    for county in queryset:
+        state_name = STATE_NAMES.get(county.state, county.state)
+        query = f"{county.name}, {state_name}, USA"
+        try:
+            location = geolocator.geocode(query)
+            if location:
+                county.latitude = location.latitude
+                county.longitude = location.longitude
+                county.save(update_fields=['latitude', 'longitude'])
+                success += 1
+            else:
+                failed += 1
+        except Exception:
+            failed += 1
+        time.sleep(1.1)  # Nominatim rate limit
+    modeladmin.message_user(
+        request,
+        f'Geocoded {success} counties, {failed} failed.',
+        messages.SUCCESS if not failed else messages.WARNING,
+    )
+
 
 @admin.register(County)
 class CountyAdmin(ModelAdmin):
-    list_display = ['name', 'state', 'petition_count']
+    list_display = ['name', 'state', 'latitude', 'longitude', 'petition_count']
     list_filter = ['state']
     search_fields = ['name']
     prepopulated_fields = {'slug': ('name',)}
+    actions = [geocode_counties]
 
     def petition_count(self, obj):
         return obj.petitions.count()
